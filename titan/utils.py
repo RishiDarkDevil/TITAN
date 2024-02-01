ff35b299b220e38cf5ce3cfe4bddeca0a0b7d515
RishiDarkDevil <rishi8001100192@gmail.com>
2024-02-01 11:40:09 +0530

    Merge same image id and cat id feature added - useful for semantic segmentation

--- titan/utils.py
+++ titan/utils.py
@@ -198,6 +198,97 @@ def merge_caption_files(
     print('Done')
   print('A successful merge!')
 
+def merge_img_cat_id(
+    annotation_file_path: str = 'Data-Generated/annotations/annotations.json'
+):
+  """
+  In the annotation file if there are images with multiple annotations for same category id.
+  This merges all of them and overwrites the input annotation file.
+  It also adds `score` parameter in the annotations if already not present.
+  """
+  # loading the annotation file
+  with open(annotation_file_path) as json_file:
+    annotation_file = json.load(json_file)
+
+  # contains the image_id, cat_id and the annotation_file['annotations'] indices corresponding to that.
+  img_cats_dict = dict()
+
+  # fills the `img_cats_dict`
+  for idx, ann in enumerate(annotation_file['annotations']):
+    if (ann['image_id'], ann['category_id']) not in img_cats_dict:
+      img_cats_dict[(ann['image_id'], ann['category_id'])] = [idx]
+    else:
+      img_cats_dict[(ann['image_id'], ann['category_id'])].append(idx)
+  
+  annotations_temp = list() # stores the merged annotations
+  ann_id = 0 # keeps track of the annotation id
+
+  # iterating over all the img_id and cat_id pairs and their respective indices in annotation_file['annotations']
+  for img_cat_id, to_be_merged in img_cats_dict.items():
+
+    # the image id and cat id
+    img_id = img_cat_id[0]
+    cat_id = img_cat_id[1]
+
+    # stores the merged annotations for the same image_id and category_id
+    segmentation = list()
+    area = 0
+    bbox = [float('inf'),float('inf'),0,0]
+    score = 0.0 
+
+    # iterating over all the indices in the original annotations that are to be merged
+    for idx in to_be_merged:
+
+      # pick the annotation to be merged and start merging process
+      ann_temp = annotation_file['annotations'][idx]
+
+      # separate contours but pertaining to the same category id in the same image are merged 
+      segmentation.extend(ann_temp['segmentation'])
+
+      # areas of these disjoint segments are added
+      area += ann_temp['area']
+
+      # sum scores if present in the dict
+      score += ann_temp.get('score', 0.0)
+
+      # making the box that is the largest size and contains all the boxes
+      box = ann_temp['bbox']
+      bbox[0] = min(bbox[0], box[0])
+      bbox[1] = min(bbox[1], box[1])
+      bbox[2] = max(bbox[2], box[0] + box[2])
+      bbox[3] = max(bbox[3], box[1] + box[3])
+    
+    # converting the xmin, ymin, xmax, ymax --> xmin, ymin, width, height
+    bbox = [int(bbox[0]), int(bbox[1]), int(bbox[2] - bbox[0]), int(bbox[3] - bbox[1])]
+
+    # incrementing annotaiton id
+    ann_id += 1
+
+    # adding the merged annotation to the annotations_temp
+    annotations_temp.append({
+      'segmentation': segmentation,
+      'area': area,
+      'iscrowd': 0,
+      'image_id': img_id,
+      'bbox': bbox,
+      'category_id': cat_id,
+      'id': ann_id,
+      'score':score / len(to_be_merged)
+    })
+
+  # putting the new annotations in place
+  annotation_file['annotations'] = annotations_temp
+
+  # Serializing json
+  json_obj_det = json.dumps(annotation_file, indent=4, cls=NpEncoder)
+
+  # Writing json
+  with open(annotation_file_path, "w") as outfile:
+    outfile.write(json_obj_det)
+
+  print(f'Saved Annotations at... {annotation_file_path}')
+  
+
 def optimize_gpu():
   """
   Frees up GPU to help reduce memory leak
